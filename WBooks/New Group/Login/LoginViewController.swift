e910e99bca2ff39cb6214c5d9cd0989c7fdc3b89
Fermin <fermin.robilotte@wolox.com.ar>
2019-04-17 16:58:08 -0300

    Primer view login-Card3.0

---
+++ WBooks/New Group/Login/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LoginViewController.swift
+//  WBooks
+//
+//  Created by Fermin Robilotte on 17/04/2019.
+//  Copyright © 2019 Wolox. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import WolmoCore
+
+final class LoginViewController: UIViewController {
+    
+    private let _loginView: LoginView = LoginView.loadFromNib()!
+    
+    override func loadView() {
+        view = _loginView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        _loginView.loginGoogle.layer.cornerRadius = 20
+        _loginView.loginGoogle.layer.borderWidth = 1.5
+        _loginView.loginGoogle.layer.borderColor = UIColor.white.cgColor
+    }
+}
